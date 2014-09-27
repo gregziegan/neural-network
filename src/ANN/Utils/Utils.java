98d7d12525a4d6634c94f17458024d1b0dbb94d8
MichaelHarkins <Mph47@case.edu>
2014-09-27 18:13:25 -0400

    Added the normalization of data

--- src/ANN/Utils/Utils.java
+++ src/ANN/Utils/Utils.java
@@ -1,11 +1,13 @@
 package ANN.Utils;
 
+import Parsing.data.DataSet;
 import Parsing.data.Instance;
+
+import java.util.ArrayList;
 
 public class Utils {
 
     public static double evaluateSigmoid(final double exponent) {
-
         return 1 / (1 + Math.exp(-exponent) );
     }
 
@@ -18,6 +20,25 @@ public class Utils {
             sum += a[i] * b[i];
         }
         return sum;
+    }
+
+    public static double normalizeData(double data, double mean, double std) {
+        return (data - mean) / std;
+    }
+
+    public static void normalizeDataSet(DataSet dataset) {
+        double[] means = new double[dataset.numAttributes()];
+        double[] stds = new double[dataset.numAttributes()];
+        for (int attribute = 1; attribute < dataset.numAttributes() -1; attribute++) {
+            means[attribute] = dataset.mean(attribute);
+            stds[attribute] = dataset.stdDeviation(attribute);
+        }
+            for (int example = 0; example < dataset.size(); example++) {
+                Instance instance = dataset.instance(example);
+                for (int attributeValue = 1; attributeValue < instance.numAttributes() - 1; attributeValue++) {
+                    instance.setValue(attributeValue, normalizeData(instance.value(attributeValue), means[attributeValue], stds[attributeValue]));
+            }
+        }
     }
 
     public static double[] getInstanceValues(Instance instance) {
