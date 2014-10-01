2f7bb3198c1d5bf91e9c8914ae7cf25ab5e68d78
Greg Ziegan <greg.ziegan@gmail.com>
2014-10-01 13:59:19 -0400

    corrected some feed forward logic for biases

--- src/ANN/Bias.java
+++ src/ANN/Bias.java
@@ -15,18 +15,7 @@ public class Bias extends Neuron {
     }
 
     @Override
-    public double getInputValue() {
-        return this.activationThreshold;
-    }
-
-    @Override
-    public void updateOutput(double[] weights, double[] inputValues) {
-        // ...
-    }
-
-    @Override
-    public double getOutputValue() {
-        if (getLayer() == Layer.INPUT)
-            super.
+    public String toString() {
+        return String.format("<Bias: layer: %s id: %d", super.id, getLayer());
     }
 }
