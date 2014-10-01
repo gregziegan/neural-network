60d61742637e84ade7ba506b128f1d2e884ad007
MichaelHarkins <Mph47@case.edu>
2014-10-01 17:18:44 -0400

    About to merge

---
+++ src/test/ANN/NetworkTest.java
@@ -0,0 +1,132 @@
+package test.ANN; 
+
+import Parsing.data.DataSet;
+import org.junit.Test;
+import org.junit.Before; 
+import org.junit.After; 
+
+
+public class NetworkTest { 
+
+
+@Before
+public void before() throws Exception {
+
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getInputNeurons() 
+* 
+*/ 
+@Test
+public void testGetInputNeurons() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getHiddenNeurons() 
+* 
+*/ 
+@Test
+public void testGetHiddenNeurons() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getOutputNeuron() 
+* 
+*/ 
+@Test
+public void testGetOutputNeuron() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getWeightDecay() 
+* 
+*/ 
+@Test
+public void testGetWeightDecay() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: trainUntilConvergence(DataSet trainingSet) 
+* 
+*/ 
+@Test
+public void testTrainUntilConvergence() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: train(DataSet trainingSet, int numberOfTrainingIterations) 
+* 
+*/ 
+@Test
+public void testTrain() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: backPropagate(double classLabel) 
+* 
+*/ 
+@Test
+public void testBackPropagate() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getNeuronInputWeights(int neuronIndex) 
+* 
+*/ 
+@Test
+public void testGetNeuronInputWeights() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getNeuronInputs(int neuronIndex) 
+* 
+*/ 
+@Test
+public void testGetNeuronInputs() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: feedForward(double[] inputLayerValues) 
+* 
+*/ 
+@Test
+public void testFeedForward() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: classify(Instance instance) 
+* 
+*/ 
+@Test
+public void testClassify() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
